74f66e660d3d2ea9b83b0a3625556a4bdaa0de10
ntlx <anmail@ukr.net>
2023-02-03 20:45:33 +0200

    workflow_dispatch

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
 serve((_req) => {
-  return new Response("<h1>Hello World!</h1> I'm NTLX@2f.co.ua", {
+  return new Response("<h1>Hello World!</h1> I'm NTLX@2f.co.ua<br>workflow_dispatch", {
     headers: { "content-type": "text/html" },
   });
 });
