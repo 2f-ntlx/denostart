1ca68a8fdca119894f29343df20461f73a5ff647
ntlx <anmail@ukr.net>
2023-02-03 20:20:52 +0200

    html res

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
 serve((_req) => {
-  return new Response("Hello World! I'm NTLX@2f.co.ua", {
-    headers: { "content-type": "text/plain" },
+  return new Response("<h1>Hello World!</h1> I'm NTLX@2f.co.ua", {
+    headers: { "content-type": "text/html" },
   });
 });
